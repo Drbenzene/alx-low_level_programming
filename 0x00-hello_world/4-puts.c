efddc3f4bf9cb3ef3fa7d33915c5c323d6471fad
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-02-05 02:26:28 +0000

    Adding Solution of Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- *
  * main - main block
- *
  * Return: 0
- *
+ * Print the Text Required using puts
  */
 
 int main(void)
 
 {
 
- puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
- return (0);
+	return (0);
 
 }

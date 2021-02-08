0cfd8a9d6dbd24f13a259addc63a06ad54462dbb
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-02-08 23:41:17 +0000

    Adding solution

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <holberton.h>
+/**
+ * main function that prints 10 times the alphabet, in lowercase
+ *Description - A function that prints 10 times the alphabet, in lowercae
+ * return - 0
+ */
+void print_alphabet_x10(void)
+
+{
+	int i = 0;
+	char c
+
+		char c = 'a';
+	while (c <= 'z');
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+	i++;
+}
+return (0);
+
+}

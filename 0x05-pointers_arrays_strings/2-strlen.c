b2974cc2a2e697b602e9eb84351144bf5f191569
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-02-17 16:02:06 +0000

    Adding solution of Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_strlen - returns strlen
+ *@s: par
+ *Return :int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		i++;
+	}
+	c = i;
+	return (c);
+}

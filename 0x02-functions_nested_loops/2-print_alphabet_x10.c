11dd75967b97c376833d5234b9c669eef369adf6
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-02-20 21:50:54 -0500

    Adding solution of completed task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include <holberton.h>
 /**
- * main function that prints 10 times the alphabet, in lowercase
- *Description - A function that prints 10 times the alphabet, in lowercae
- * return - 0
+ * print_alphabet_x10 - print entire alphabet on 10 lines
+ *
+ * Return: Void.
  */
 void print_alphabet_x10(void)
 
 {
 	int i = 0;
-	char c
-
+	char c;
 		char c = 'a';
-	while (c <= 'z');
+	while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
@@ -20,6 +19,5 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 	i++;
 }
-return (0);
 
 }

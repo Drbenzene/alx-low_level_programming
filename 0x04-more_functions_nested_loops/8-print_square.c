0c47896f0bb4e0ce527fd712db8295c40d4d5789
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-03-05 09:11:24 -0500

    Adding solution

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,24 @@
 #include "holberton.h"
-/**
-* print_square - print a nxn square
-*
-*@size: length & Width of square
-*
-*Return: void
-*/
+#include <stdio.h>
 
-void print_square(int size)
+/**
+ * print_square - Print square line dependent on the integer n.
+ * @n : The number of lines using '#' characters to use per row and column
+ * Return: Void.
+ */
+void print_square(int n)
 {
 	int x;
 	int y;
 
-	for (y = 0; y < size; y++)
+	for (y = 0; y < n; y++)
 	{
-		for (x =0; x < size; x++)
+		for (x = 0; x < n; x++)
 		{
 			_putchar('#');
 		}
+		_putchar('\n');
 	}
-
 	if (n <= 0)
 	{
 		_putchar('\n');

051ff74c5f3218738b8c00db98747db8996bc14d
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-03-05 09:43:32 -0500

    Adding Solution

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,20 +1,17 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
-* print_triangle - Print Triangle
-* @size - Size of the Triangle
-* @_putchar - To print
-* @size : The heigh of the triangle
-* @# - Print Triangle
-* Return - void
-*/
-
-void print_triangle(int size);
-
+ * print_triangle - Print triangle dependent on the parameter size.
+ * @size : The height of the triangle
+ * Return: Void.
+ */
+void print_triangle(int size)
 {
 	int i;
 	int x;
 	int spaces;
-
+;
 	for (i = 0; i < size; i++)
 	{
 		for (spaces = size - 1 - i; spaces > 0; spaces--)

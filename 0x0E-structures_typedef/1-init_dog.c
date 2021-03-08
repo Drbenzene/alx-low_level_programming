679773a746a23f784107abc484d1c2fc314b5891
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2021-03-08 03:55:41 -0500

    Adding solution

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+* init_dog - Creaye variable dog
+* @d: pointer to dog struct
+* @name: name of dog
+* @age: age of dog
+* @owner: the owner of the dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

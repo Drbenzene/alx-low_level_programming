386b0412374c0a28f7771bfad3807e5c310427b2
Boyinbode Ebenezer Ayomide <enegxi@gmail.com>
2021-07-28 04:41:28 -0700

    Updating Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,37 @@ include "holberton.h"
 
 void times_table(void)
 {
-	int x;
-	int y;
+	int x = 0;
+	int y = 0;
 	int multiplied_value;
 
-	for (y = 0; y < 10; y++)
-	{
-		multiplied
+	while (x < 10)
+	  {
+	    while (y < 10)
+	      {
+		multiplied_value = x * y;
+		
+		if (multiplied_value <=9 && y != 0)
+		  {
+		    _putchar(' ');
+		  }
+		if (multiplied_value > 9)
+		  {
+		    _putchar(multiplied_value / 10 + '0');
+		    _putchar(multiplied_value % 10 + '0');
+		  }
+		else
+		  {
+		    _puchar(multiplied_value + '0');
+		  }
+		if (y != 9)
+		    _putchar(',');
+		    _putchar(' ');
+		  }
+		y++;
+	      }
+	    _putchar('\n');
+	    x++;
+	    y = 0;
+}
+}
